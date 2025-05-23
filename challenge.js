d962c89b52bf3be3bf0c59be695407e7e8715bb6
Sam Shock <sam.shock924@gmail.com>
2025-05-22 23:35:22 -0400

    first commit

---
+++ challenge.js
@@ -0,0 +1,25 @@
+// May 22 Challenge: Create an inventory system for a game
+// const categoryItems = ['weapon', 'potion', 'armor'];
+// const inventory = [
+// 	{name: 'sword', quantity: 3, category: categoryItems[0]},
+// 	{name: 'health potion', quantity: 5, category: categoryItems[1]},
+// 	{name: 'shield', quantity: 2, category: categoryItems[2]},
+// 	{name: 'mana potion', quantity: 4, category: categoryItems[1]},
+// 	{name: 'bow', quantity: 1, category: categoryItems[0]},
+// ];
+
+// function addItem(name, quantity, category) {
+// 	const item = inventory.find((item) => item.name === name);
+// 	if (item) {
+// 		item.quantity += quantity;
+// 	} else {
+// 		inventory.push({name, quantity, category});
+// 	}
+// }
+
+// function filterByCategory(category) {
+// 	return inventory.filter((item) => item.category === category);
+// }
+
+// console.log(inventory.map((item) => item.name));
+// console.log(inventory.map((item) => item.quantity));
