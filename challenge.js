72b48f9e168d8a897d4e90d66f3880ffb16baf4d
Sam Shock <sam.shock924@gmail.com>
2025-05-24 14:31:14 -0400

    new challenge

--- challenge.js
+++ challenge.js
@@ -36,3 +36,71 @@
 // }
 
 // isEven(null);
+
+/*
+Build a very simple “task manager” system.
+
+Each task should have:
+	•	title (string)
+	•	completed (boolean)
+	•	priority (number, 1 = high, 3 = low)
+
+Your goals:
+	1.	Store at least 5 tasks in an array
+	2.	Write a function that:
+	•	Marks a task as completed by its title
+	3.	Write a function that:
+	•	Filters tasks by priority
+
+Bonus: Write a function that sorts tasks by priority (1 first)
+*/
+
+// const taskList = [
+// 	{task: 'laundry', completed: true, priority: 3},
+// 	{task: 'coding challenge', completed: false, priority: 1},
+// 	{task: 'eat lunch', completed: true, priority: 2},
+// 	{task: 'create badass future', completed: false, priority: 1},
+// 	{task: 'relax', completed: false, priority: 3},
+// ];
+
+// function completeTask(taskName) {
+// 	const task = taskList.find((t) => t.task === taskName);
+// 	if (!task) {
+// 		console.log('Task not found');
+// 		return;
+// 	}
+// 	if (task.completed) {
+// 		console.log('Task already complete!');
+// 		return;
+// 	}
+// 	console.log('Task incomplete... completing now');
+// 	setTimeout(() => {
+// 		task.completed = true;
+// 		console.log('Task now complete!');
+// 	}, 3000);
+// }
+
+// completeTask('coding challenge');
+
+/*
+Write a function that takes a number and returns:
+	•	"Fizz" if it’s divisible by 3
+	•	"Buzz" if it’s divisible by 5
+	•	"FizzBuzz" if divisible by both
+	•	"Even" if it’s not divisible by 3 or 5, but it’s even
+	•	"Odd" if it’s none of the above
+*/
+
+// function check(number) {
+// 	if (number % 3 == 0 && number % 5 == 0) {
+// 		console.log('FizzBuzz');
+// 	} else if (number % 3 == 0) {
+// 		console.log('Fizz');
+// 	} else if (number % 5 == 0) {
+// 		console.log('Buzz');
+// 	} else if (number % 2 == 0) {
+// 		console.log('Even');
+// 	} else console.log('Odd');
+// }
+
+// check(30);
